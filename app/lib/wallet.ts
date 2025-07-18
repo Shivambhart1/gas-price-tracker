b8e10cb572c72e812b003507bc94d2ec6edc1a51
Shivambhart1 <shivama.21.becs@acharya.ac.in>
2025-07-19 00:03:10 +0530

    remove wallet.ts

--- app/lib/wallet.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from "ethers";
-
-export const getBrowserProvider = () => {
-  if (typeof window === "undefined" || !window.ethereum) {
-    throw new Error("No injected wallet found");
-  }
-
-  return new ethers.BrowserProvider(window.ethereum);
-};
